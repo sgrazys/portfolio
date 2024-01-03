292282dacc351f9a8cd1368c5668eaab941bc265
sgrazys <s.grazys@gmail.com>
2024-01-03 19:27:31 +0200

    add data flie

---
+++ src/myData.js
@@ -0,0 +1,42 @@
+const myData = {
+	socialMedia: [
+		{ name: 'GitHub', url: 'https://github.com/sgrazys', icon: '/assets/images/icon-github.svg' },
+		{ name: 'FrontendMentor', url: 'https://www.frontendmentor.io/profile/sgrazys', icon: '/assets/images/icon-frontend-mentor.svg' },
+		{ name: 'LinkedIn', url: 'https://www.linkedin.com/in/saulius-grazys/', icon: '/assets/images/icon-linkedin.svg' },
+	],
+
+	skills: [
+		{ skill: 'HTML', level: 'Intermediate' },
+		{ skill: 'CSS', level: 'Intermediate' },
+		{ skill: 'JavaScript', level: 'Beginner' },
+		{ skill: 'React', level: 'Beginner' },
+		{ skill: 'Sass', level: 'Beginner' },
+		{ skill: 'SQL', level: 'Basics' },
+		{ skill: 'Git', level: 'Basics' },
+		{ skill: 'GitHub', level: 'Intermediate' },
+		{ skill: 'Software Testing', level: 'Basics' },
+		{ skill: 'Agile', level: 'Basics' },
+	],
+};
+
+export default myData;
+
+/*
+{
+	name: 'GitHub',
+	url: 'https://github.com/sgrazys',
+	icon: '/assets/images/icon-github.svg',
+},
+
+{
+	name: 'FrontendMentor',
+	url: 'https://www.frontendmentor.io/profile/sgrazys',
+	icon: '/assets/images/icon-frontend-mentor.svg',
+},
+
+{
+	name: 'LinkedIn',
+	url: 'https://www.linkedin.com/in/saulius-grazys/',
+	icon: '/assets/images/icon-linkedin.svg',
+},
+*/
