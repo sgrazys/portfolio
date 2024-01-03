f496db2e8a5064586f2e6760864c7220b44610aa
sgrazys <s.grazys@gmail.com>
2024-01-03 19:29:45 +0200

    small updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
-import data from '../data.js';
-
+import myData from '../myData';
 import Header from './Header';
+import { v4 as uuidv4 } from 'uuid';
+
+const { socialMedia } = myData;
+const socialmediaData = socialMedia.map((el) => ({ ...el, id: uuidv4() }));
 
 function App() {
 	return (
 		<div className='App'>
-			<Header />
+			<Header socialmediaList={socialmediaData} />
 		</div>
 	);
 }
