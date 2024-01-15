7fdaff092fca6cd0943c23edb7a59222ca12587e
sgrazys <s.grazys@gmail.com>
2024-01-15 18:53:27 +0200

    messing around

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -74,29 +74,15 @@ function Form() {
 						name='form-name'
 						value='contact'
 					/>
-					<p>
-						<label>
-							Your Name:{' '}
-							<input
-								type='text'
-								name='name'
-							/>
-						</label>
-					</p>
-					<p>
-						<label>
-							Your Email:{' '}
-							<input
-								type='email'
-								name='email'
-							/>
-						</label>
-					</p>
-					<p>
-						<label>
-							Message: <textarea name='message'></textarea>
-						</label>
-					</p>
+					<input
+						type='text'
+						name='name'
+					/>
+					<input
+						type='email'
+						name='email'
+					/>
+					Message: <textarea name='message'></textarea>
 					<p>
 						<button type='submit'>Send</button>
 					</p>
